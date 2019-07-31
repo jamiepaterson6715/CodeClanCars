a6f7f2e2a5ee0b2cd3b1bbae42830f4eeba4aed0
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-07-31 13:37:32 +0100

    Created and tested tyres class

--- src/main/java/components/Tyres.java
+++ src/main/java/components/Tyres.java
@@ -1,4 +1,30 @@
 package components;
 
 public class Tyres {
+
+
+    private int width;
+    private int aspectRatio;
+    private int diameter;
+
+    public Tyres(int width, int aspectRatio, int diameter) {
+        this.width = width;
+        this.aspectRatio = aspectRatio;
+        this.diameter = diameter;
+    }
+
+    // Getters
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getAspectRatio() {
+        return aspectRatio;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
 }
--- src/test/java/components/TyresTest.java
+++ src/test/java/components/TyresTest.java
@@ -1,4 +1,32 @@
 package components;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyresTest {
+
+    Tyres tyres;
+
+    @Before
+    public void before() {
+        tyres = new Tyres(205, 60, 15);
+    }
+
+    @Test
+    public void canGetWidth() {
+        assertEquals(205, tyres.getWidth());
+    }
+
+    @Test
+    public void canGetAspectRatio() {
+        assertEquals(60, tyres.getAspectRatio());
+    }
+
+    @Test
+    public void canGetDiameter() {
+        assertEquals(15, tyres.getDiameter());
+    }
+
 }
