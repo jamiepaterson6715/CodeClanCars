d6d17398cd0e6f9f5437a0b68e9a6e112060716d
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-07-31 13:49:58 +0100

    Gearbox/GBTest added

--- src/main/java/components/Gearbox.java
+++ src/main/java/components/Gearbox.java
@@ -1,4 +1,15 @@
 package components;
 
 public class Gearbox {
+
+    private GearboxType gearboxType;
+
+    public Gearbox (GearboxType gearboxtype) {
+
+        this.gearboxType = gearboxtype;
+    }
+
+    public GearboxType getGearboxType() {
+        return gearboxType;
+    }
 }
--- src/test/java/components/GearboxTest.java
+++ src/test/java/components/GearboxTest.java
@@ -1,4 +1,24 @@
 package components;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GearboxTest {
+
+    Gearbox gearbox;
+
+    @Before
+    public void setUp() throws Exception {
+       gearbox = new Gearbox(GearboxType.AUTOMATIC);
+    }
+
+    @Test
+    public void canGetGearboxType() {
+        assertEquals(GearboxType.AUTOMATIC, gearbox.getGearboxType());
+    }
+
+
+
 }
